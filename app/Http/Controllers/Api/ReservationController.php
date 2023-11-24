5f7a0d328c4df102172800bfc9f241320937e5b5
shahvaizahmed29 <shahvaiz@akvateq.com>
2023-11-24 15:59:15 +0500

    added pagination

--- app/Http/Controllers/Api/ReservationController.php
+++ app/Http/Controllers/Api/ReservationController.php
@@ -13,12 +13,36 @@ class ReservationController extends BaseController
 {
     public function getReservationsByMember($member_id = null) {
         $reservations = Reservation::with(['session', 'session.programLevel','session.programLevel.program'])->where('member_id', 1)->paginate(1);
-        return $this->sendResponse(ReservationResource::collection($reservations), 'Success');
+        $data = [
+            'reservations' => ReservationResource::collection($reservations),
+            'pagination' => [
+                'current_page' => $reservations->currentPage(),
+                'per_page' => $reservations->perPage(),
+                'total' => $reservations->total(),
+                'prev_page_url' => $reservations->previousPageUrl(),
+                'next_page_url' => $reservations->nextPageUrl(),
+                'first_page_url' => $reservations->url(1),
+                'last_page_url' => $reservations->url($reservations->lastPage()),
+            ],
+        ];
+        return $this->sendResponse($data, 'Get member reservations successfully');
     }
 
     public function getCheckInsByReservation($reservation_id) {
         $checkIns = CheckIn::where('id', $reservation_id)->paginate(1);
-        return $this->sendResponse(CheckInResource::collection($checkIns), 'Success');
+        $data = [
+            'checkIns' => ReservationResource::collection($checkIns),
+            'pagination' => [
+                'current_page' => $checkIns->currentPage(),
+                'per_page' => $checkIns->perPage(),
+                'total' => $checkIns->total(),
+                'prev_page_url' => $checkIns->previousPageUrl(),
+                'next_page_url' => $checkIns->nextPageUrl(),
+                'first_page_url' => $checkIns->url(1),
+                'last_page_url' => $checkIns->url($checkIns->lastPage()),
+            ],
+        ];
+        return $this->sendResponse($data, 'Get checkins by reservation');
     }
 
     public function markAttendance(Request $request){
@@ -35,5 +59,4 @@ class ReservationController extends BaseController
         $checkIn->save();
         return $this->sendResponse(new CheckInResource($checkIn), 'Success');
     }
-    
 }
