18d600ae936bb5f070707a147114f87df7693f07
ashirakvateq <ashir@akvateq.com>
2023-12-08 17:06:59 +0500

    Now only loggedin vendor will be able authenticate in the vendor authenticate api.

--- app/Http/Controllers/Api/Vendor/AuthController.php
+++ app/Http/Controllers/Api/Vendor/AuthController.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Log;
 class AuthController extends BaseController
 {
     public function vendorAuthenticate(Request $request){
+        if($request->user()->email !== $request->email){
+            return $this->sendResponse('Unauthorized', 401);
+        }
+
         $vendor = User::where('email', $request->email)->whereHas("roles", function ($q){
             $q->where('name', \App\Models\User::VENDOR);
         })->first();
