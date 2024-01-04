fd547d9bb400676439df48da9c07554bda2fb2d3
sarimzia <sarim@akvateq.com>
2024-01-04 13:24:02 +0000

    fix in program update api.

--- app/Http/Controllers/Api/Vendor/ProgramController.php
+++ app/Http/Controllers/Api/Vendor/ProgramController.php
@@ -171,6 +171,7 @@ class ProgramController extends BaseController
                     ]
                 );
 
+                $programLevel->custom_field_ghl_value = $program->name . '_' . $programLevel->id;
                 $programLevel->save();
 
                 $sessionId = isset($session['id']) ? $session['id'] : null;
