08b105a6d873a8df1b4b9467e881f49b9b68ccec
ashirakvateq <ashir@akvateq.com>
2024-01-16 19:57:48 +0500

    Fix in getVendorProfile resource.

--- app/Http/Resources/Vendor/GetVendorProfile.php
+++ app/Http/Resources/Vendor/GetVendorProfile.php
@@ -14,22 +14,27 @@ class GetVendorProfile extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $vendor = [
-            'id' => isset($this->id)? $this->id : null,
-            'name' => isset($this->name)? $this->name : null,
-            'email' => isset($this->email)? $this->email : null,
-            'firstName' => isset($this->vendor->first_name)? $this->vendor->first_name : null,
-            'lastName' => isset($this->vendor->last_name)? $this->vendor->last_name : null,
-            'phoneNumber' => isset($this->vendor->phone_number)? $this->vendor->phone_number : null,
-            'companyName' => isset($this->vendor->company_name)? $this->vendor->company_name : null,
-            'companyEmail' => isset($this->vendor->company_email)? $this->vendor->company_email : null,
-            'companyWebsite' => isset($this->vendor->company_website)? $this->vendor->company_website : null,
-            'companyAddress' => isset($this->vendor->company_address)? $this->vendor->company_address : null,
-            'logo' => isset($this->vendor->logo)? $this->vendor->logo : null,
-            'planName' => $this->vendor->plan->name,
-            'pin' => isset($this->vendor->pin)? $this->vendor->pin : null, 
-            'vendorId' => isset($this->vendor->id)? $this->vendor->id : null,
-        ];
+        $vendor = [];
+
+        if ($this->resource) {
+            $vendor = [
+                'id' => $this->resource->id,
+                'name' => $this->resource->name,
+                'email' => $this->resource->email,
+                'firstName' => optional($this->resource->vendor)->first_name,
+                'lastName' => optional($this->resource->vendor)->last_name,
+                'phoneNumber' => optional($this->resource->vendor)->phone_number,
+                'companyName' => optional($this->resource->vendor)->company_name,
+                'companyEmail' => optional($this->resource->vendor)->company_email,
+                'companyWebsite' => optional($this->resource->vendor)->company_website,
+                'companyAddress' => optional($this->resource->vendor)->company_address,
+                'logo' => optional($this->resource->vendor)->logo,
+                'planName' => optional(optional($this->resource->vendor)->plan)->name,
+                'pin' => optional($this->resource->vendor)->pin,
+                'vendorId' => optional($this->resource->vendor)->id,
+            ];
+        }
+
         return $vendor;
     }
 }
